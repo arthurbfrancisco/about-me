ef0e3806032b41f9d3903a47cdbcbf5e4018d3c8
Daniel Quinn <dandidit.quinn@gmail.com>
2023-04-06 14:26:26 -0700

    visit

--- js/app.js
+++ js/app.js
@@ -8,61 +8,65 @@ alert(`Hello ${nickname}, nice to meet you.`); // template literal that is used 
 let city = prompt('Where are you from?');
 alert(`I've never been there, but it must be a great place to travel to, ${city}.`);
 
-function birthQ(){
-let placeOfBirth = prompt('Was I born in California?');
-console.log(placeOfBirth);
-if (placeOfBirth === 'y' || placeOfBirth === 'yes') {
-  alert('No, I was actually born in the Philippines but raised in America.');
-} else if (placeOfBirth === 'n' || placeOfBirth === 'no') {
-  alert('You are correct. I was born in the Philippines and I am a first generation immigrant.');
-} else {
-  alert('Please answer with a "yes" or "no".');
-}
+function birthQ() {
+  let placeOfBirth = prompt('Was I born in California?');
+  console.log(placeOfBirth);
+  if (placeOfBirth === 'y' || placeOfBirth === 'yes') {
+    alert('No, I was actually born in the Philippines but raised in America.');
+  } else if (placeOfBirth === 'n' || placeOfBirth === 'no') {
+    alert('You are correct. I was born in the Philippines and I am a first generation immigrant.');
+  } else {
+    alert('Please answer with a "yes" or "no".');
+  }
 }
 
 birthQ;
 
-function horrorQ(){
-let likeHorrorFlick = prompt('Do I like watching horror movies? Yes or No?').toLowerCase();
+function horrorQ() {
+  let likeHorrorFlick = prompt('Do I like watching horror movies? Yes or No?').toLowerCase();
 
-if (likeHorrorFlick === 'y' || likeHorrorFlick === 'yes'.toLowerCase()) {
-  alert('You are right, I love watching horror movies.');
-} else if (likeHorrorFlick === 'n' || likeHorrorFlick === 'no') {
-  alert('You are wrong. I enjoy horror movies.');
-} else {
-  alert('You need to answer with \'yes\' or \'no\'.');
-}
+  if (likeHorrorFlick === 'y' || likeHorrorFlick === 'yes'.toLowerCase()) {
+    alert('You are right, I love watching horror movies.');
+  } else if (likeHorrorFlick === 'n' || likeHorrorFlick === 'no') {
+    alert('You are wrong. I enjoy horror movies.');
+  } else {
+    alert('You need to answer with \'yes\' or \'no\'.');
+  }
 }
 
 horrorQ();
 
+function visitQ() {
+  // High low, guessing 
+  let placesVisited = 6;
 
-// // High low, guessing 
-// let placesVisited = 6;
+  // They have 6 attempts
+  let guessCount = 6;
 
-// // They have 6 attempts
-// let guessCount = 6;
+  // Loop and guess again, while loop will continue to run
+  while (guessCount > 0) {
+    // Get the user's guess
+    let guess = parseInt(prompt('How many places have I visited between 1-7?'));
 
-// // Loop and guess again, while loop will continue to run
-// while (guessCount > 0) {
-//   // Get the user's guess
-//   let guess = parseInt(prompt('How many places have I visited between 1-7?'));
+    // Convert to digits
+    guess = parseInt(guess);
 
-//   // Convert to digits
-//   guess = parseInt(guess);
+    // Check if the guess is correct
+    if (guess === placesVisited) {
+      alert('Correct! You guessed the number of places I have visited.');
+      break; // it will break to next condition
+    } else if (guess < placesVisited) {
+      guessCount--; // subtract attempt
+      alert(`Incorrect. The number of places I have visited is higher. You have ${guessCount} guesses left.`);
+    } else if (guess > placesVisited) {
+      guessCount--;
+      alert(`Incorrect. The number of places I have visited is lower. You have ${guessCount} guesses left.`);
+    }
+  }
+}
 
-//   // Check if the guess is correct
-//   if (guess === placesVisited) {
-//     alert('Correct! You guessed the number of places I have visited.');
-//     break; // it will break to next condition
-//   } else if (guess < placesVisited) {
-//     guessCount--; // subtract attempt
-//     alert(`Incorrect. The number of places I have visited is higher. You have ${guessCount} guesses left.`);
-//   } else if (guess > placesVisited) {
-//     guessCount--;
-//     alert(`Incorrect. The number of places I have visited is lower. You have ${guessCount} guesses left.`);
-//   }
-// }
+visitQ();
+
 // // Input correct answer
 // alert(`The correct number of places I have visited is ${placesVisited}. These places are Thailand, Vietnam, Cambodia, Bali, Philippines, and South Korea.`);
 
