c2d58912340f1392df50cd2e60c7457a01a7886a
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-04-07 21:41:53 +0700

    update

--- js/app.js
+++ js/app.js
@@ -7,106 +7,121 @@ alert(`Hello ${nickname}, nice to meet you.`); // template literal that is used 
 
 let city = prompt('Where are you from?');
 alert(`I've never been there, but it must be a great place to travel to, ${city}.`);
-//Places 
-let placeOfBirth = prompt('Was I born in California?');
-console.log(placeOfBirth);
 
-if (placeOfBirth === 'y' || placeOfBirth === 'yes') {
-  alert('No, I was actually born in the Philippines but raised in America.');
-} else if (placeOfBirth === 'n' || placeOfBirth === 'no') {
-  alert('You are correct. I was born in the Philippines and I am a first generation immigrant.');
-} else {
-  alert('Please answer with a "yes" or "no".');
-}
-
-// Horrors
-let likeHorrorFlick = prompt('Do I like watching horror movies? Yes or No?').toLowerCase();
-
-if (likeHorrorFlick === 'y' || likeHorrorFlick === 'yes'.toLowerCase()) {
-  alert('You are right, I love watching horror movies.');
-} else if (likeHorrorFlick === 'n' || likeHorrorFlick === 'no') {
-  alert('You are wrong. I enjoy horror movies.');
-} else {
-  alert('You need to answer with \'yes\' or \'no\'.');
-}
-//Guess the number of places 
-// High low, guessing 
-let placesVisited = 6;
-
-// They have 6 attempts
-let guessCount = 6;
-
-// Loop and guess again, while loop will continue to run
-while (guessCount > 0) {
-  // Get the user's guess
-  let guess = parseInt(prompt('How many places have I visited between 1-7?'));
-
-  // Convert to digits
-  guess = parseInt(guess);
-
-  // Check if the guess is correct
-  if (guess === placesVisited) {
-    alert('Correct! You guessed the number of places I have visited.');
-    break; // it will break to next condition
-  } else if (guess < placesVisited) {
-    guessCount--; // subtract attempt
-    alert(`Incorrect. The number of places I have visited is higher. You have ${guessCount} guesses left.`);
-  } else if (guess > placesVisited) {
-    guessCount--;
-    alert(`Incorrect. The number of places I have visited is lower. You have ${guessCount} guesses left.`);
+function birthQ() {
+  let placeOfBirth = prompt('Was I born in California?');
+  console.log(placeOfBirth);
+  if (placeOfBirth === 'y' || placeOfBirth === 'yes') {
+    alert('No, I was actually born in the Philippines but raised in America.');
+  } else if (placeOfBirth === 'n' || placeOfBirth === 'no') {
+    alert('You are correct. I was born in the Philippines and I am a first generation immigrant.');
+  } else {
+    alert('Please answer with a "yes" or "no".');
   }
 }
-// Input correct answer
-alert(`The correct number of places I have visited is ${placesVisited}. These places are Thailand, Vietnam, Cambodia, Bali, Philippines, and South Korea.`);
+// Bday
+birthQ;
 
+function horrorQ() {
+  let likeHorrorFlick = prompt('Do I like watching horror movies? Yes or No?').toLowerCase();
 
-// What is my favorite ice cream
-
-let myFavIceCream = 'rocky road'; // starting in the beginning
-
-let attemptsRemaining = 6;
-let userResponse = prompt('What is my favorite ice cream flavor? Rocky Road, Vanilla, Chocolate, Strawberry, or Mint Chocolate, or Banana Split');
-
-while (attemptsRemaining > 0 && myFavIceCream !== userResponse) {
-  attemptsRemaining--; //how many attempts left
-  console.log(`You have ${attemptsRemaining} attempts remaining.`);
-
-
-  if (myFavIceCream === userResponse) {
-    console.log('You are right!');
+  if (likeHorrorFlick === 'y' || likeHorrorFlick === 'yes'.toLowerCase()) {
+    alert('You are right, I love watching horror movies.');
+  } else if (likeHorrorFlick === 'n' || likeHorrorFlick === 'no') {
+    alert('You are wrong. I enjoy horror movies.');
+  } else {
+    alert('You need to answer with \'yes\' or \'no\'.');
   }
 }
-if (attemptsRemaining === 0) {
-  console.log(`Sorry, you are out of attempts. My favorite ice cream flavor is ${myFavIceCream}.`);
+// Horror
+horrorQ();
+
+function visitQ() {
+  // High low, guessing 
+  let placesVisited = 6;
+
+  // They have 6 attempts
+  let guessCount = 6;
+
+  // Loop and guess again, while loop will continue to run
+  while (guessCount > 0) {
+    // Get the user's guess
+    let guess = parseInt(prompt('How many places have I visited between 1-7?'));
+
+    // Convert to digits
+    guess = parseInt(guess);
+
+    // Check if the guess is correct
+    if (guess === placesVisited) {
+      alert('Correct! You guessed the number of places I have visited.');
+      break; // it will break to next condition
+    } else if (guess < placesVisited) {
+      guessCount--; // subtract attempt
+      alert(`Incorrect. The number of places I have visited is higher. You have ${guessCount} guesses left.`);
+    } else if (guess > placesVisited) {
+      guessCount--;
+      alert(`Incorrect. The number of places I have visited is lower. You have ${guessCount} guesses left.`);
+    }
+    // Input correct answer
+    alert(`The correct number of places I have visited is ${placesVisited}. These places are Thailand, Vietnam, Cambodia, Bali, Philippines, and South Korea.`);
+
+  }
 }
 
-//Food Fav
+visitQ();
+// Ice cream
+function iceQ() {
+  // What is my favorite ice cream
+
+  let myFavIceCream = 'rocky road'; // starting in the beginning
+
+  let attemptsRemaining = 6;
+
+  let userResponse = prompt('What is my favorite ice cream flavor? Rocky Road, Vanilla, Chocolate, Strawberry, or Mint Chocolate, or Banana Split');
+  while (attemptsRemaining > 0 && myFavIceCream !== userResponse) {
+    attemptsRemaining--; //how many attempts left
+    console.log(`You have ${attemptsRemaining} attempts remaining.`);
+    if (myFavIceCream === userResponse) {
+      console.log('You are right!');
+    }
+  }
+  if (attemptsRemaining === 0) {
+    console.log(`Sorry, you are out of attempts. My favorite ice cream flavor is ${myFavIceCream}.`);
+  }
+}
+
+iceQ();
+
+
+//Food fav
+
+//Food
 function foodQ() {
   let attempts = 6;
-  let topFood = ['Mexican', 'Chinese', 'Indian', 'Italian'];
-  let userPoints = 0;
+  let topFood = [‘Mexican’, ‘Chinese’, ‘Indian’,'Italian'];
   let correct = false;
   while (attempts >= 1 && !correct) {
     attempts--;
-    let foodAnswer = prompt('What is one of my favorite dishes?');
-    for (let i = 0; i < topFood.length; i++) {
-      if (foodAnswer.toLowerCase() === topFood[i].toLowerCase()) {
-        alert('Correct! I love ' + topFood[i] + ' food!');
+    let foodAnswer = prompt(‘What is one of my favorite dish?’);
+    for (let i = 0; i <foodAnswer.length; i++) {
+      if (foodAnswer === foodAnswer[i]) {
+        alert(‘Correct! I love Mexican Food!’);
         userPoints++;
         correct = true;
         break;
       }
     }
     if (attempts === 1) {
-      alert('You are out of guesses!');
+      alert(‘You are wrong, but your are getting closer!’);
       break;
     }
     if (!correct) {
-      alert('Sorry, that is not one of my favorite dishes. Please try again.');
+      alert(‘Sorry but you need to try again!’);
     }
   }
-  alert('You got ' + userPoints + ' point(s)!');
 }
+foodQ();
+
 console.log(placeOfBirth);
 console.log(likeHorrorFlick);
 console.log(myFavIceCream);
