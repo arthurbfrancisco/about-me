922dedcdde771b7dc26d954461d0d91d0567c97a
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-04-16 05:51:36 +0700

    update

--- js/app.js
+++ js/app.js
@@ -4,21 +4,30 @@
 
 alert('Hi, welcome to my page, friend!');
 
-let nickname = prompt('What is your nickname?'); // Prompt user
-alert(`Hello ${nickname}, nice to meet you.`); // template literal that is used for string interpolation
+let userName = prompt('What is your kname?'); // Prompt user
+alert(`Hello ${userName}, nice to meet you. Let me ask you a few question if you dont mind so we can get to know each other more`); // template literal that is used for string interpolation
 
-let city = prompt('Where are you from?');
-alert(`I've never been there, but it must be a great place to travel to, ${city}.`);
-
-let intro = prompt('Let me ask you a few question, if you dont mind.'); // Prompt user
-alert(`Okay, ${nickname}, here a few question to get you started to get to know me.`); // template literal that is used for string interpolation
-
+let city = prompt('First, where are you from?');
+alert(`I've never been there, but it must be a great place to travel to, ${city}.Lets get started!`); // template literal that is used for string interpolation
+//City Born
+function birthQ() {
+  let placeOfBirth = prompt('Was I born in California?');
+  console.log(placeOfBirth);
+  if (placeOfBirth === 'y' || placeOfBirth === 'yes') {
+    alert('No, I was actually born in the Philippines but raised in America.');
+  } else if (placeOfBirth === 'n' || placeOfBirth === 'no') {
+    alert('You are correct. I was born in the Philippines and I am a first generation immigrant.');
+  } else {
+    alert('Please answer with a "yes" or "no".');
+  }
+}
+birthQ();
 // CoffeeScript
 function coffeeQ() {
-  let drinkCoffee = prompt('Do I drink coffee regularly? Yes or No?').toLowerCase();
+  let drinkCoffee = prompt('First question, do I drink coffee regularly? Yes or No?').toLowerCase();
 
   if (drinkCoffee === 'y' || drinkCoffee === 'yes'.toLowerCase()) { // Lowercase will be valued even if uppercase was input
-    alert('You are correct, I drink coffee every day.');
+    alert('You are correct, I drink coffee every day. I love it!');
   } else if (drinkCoffee === 'n' || drinkCoffee === 'no') {
     alert('Incorrect, I am a regular coffee drinker.');
   } else {
@@ -26,27 +35,6 @@ function coffeeQ() {
   }
 }
 coffeeQ();
-//visited
-function visitQ() {
-  let guessCount = 0;
-
-  while (guessCount > 0) {
-    let guess = prompt('Have I visited more than 5 countries? Yes or No? (Answer with "Y" or "N")');
-
-    if (guess === 'y' || guess === 'Y' || guess === 'yes' || guess === 'Yes') {
-      alert('Correct! I have visited 6 countries.');
-      break;
-    } else if (guess === 'n' || guess === 'N' || guess === 'no' || guess === 'No') {
-      guessCount--;
-      alert(`Incorrect. I have visited more than 5 countries. You have ${guessCount} guesses left.`);
-    } else {
-      alert('Please answer with "Yes" or "No".');
-    }
-
-    alert('The correct answer is "Yes". Next question!.');
-  }
-}
-visitQ();
 
 //Height
 function heightQ() {
@@ -75,55 +63,55 @@ function horrorQ() {
   }
 }
 horrorQ();
-//Icecream Fav
+//icecream
 function iceQ() {
-  // Declare my favorite ice cream flavor
-  let myFavIceCream = 'rocky road'; // starting in the beginning
+  let myFavIceCream = 8; // my score on how much I love ice cream on a scale of 1-10
+  let attemptsRemaining = 6; // They have 6 attempts
 
-  // Declare the number of attempts the user has to answer the question correctly
-  let attemptsRemaining = 6; //attempt to try
+  // Loop and guess again, while loop will continue to run
+  while (attemptsRemaining > 0) {
+    // Get the user's guess
+    let guess = parseInt(prompt('On a scale of 1-10, how much do I love ice cream? (1 being the lowest and 10 being the highest)'));
 
-  // Display a prompt dialog asking the user to guess the favorite ice cream flavor, and store the answer in a variable
-  let userResponse = prompt('What is my favorite ice cream flavor? Rocky Road, Vanilla, Chocolate, Strawberry, or Mint Chocolate, or Banana Split');
-
-  // Declare variables to keep track of the user's score and the total number of possible points
-  let userPoints = 0;
-  let possiblePoints = 1;
-
-  // Loop while the user has remaining attempts and has not yet answered the question correctly
-  while (attemptsRemaining > 0 && myFavIceCream !== userResponse) {
-    attemptsRemaining--; // Decrease the number of attempts left
-    alert(`You have ${attemptsRemaining} attempts remaining.`); // Display the number of attempts remaining
-
-    // Check if the user's answer matches the favorite ice cream flavor
-    if (myFavIceCream === userResponse) {
-      alert('You are right!'); // Display a message indicating that the answer is correct
-      userPoints += possiblePoints; // Increment the user's score
-    } else if (attemptsRemaining > 0) {
-      // Display an alert message indicating that the answer is incorrect and prompt the user to try again
-      userResponse = prompt(`Sorry, ${userResponse} is not my favorite ice cream flavor. You have ${attemptsRemaining} attempts remaining. Please try again.`);
+    // Check if the guess is correct
+    if (guess === myFavIceCream) {
+      alert('You guessed my score correctly! I love ice cream a lot.');
+      break;
+    } else if (guess < myFavIceCream) {
+      attemptsRemaining--;
+      alert(`Your guess is too low. You have ${attemptsRemaining} attempts left.`);
+    } else if (guess > myFavIceCream) {
+      attemptsRemaining--;
+      alert(`Your guess is too high. You have ${attemptsRemaining} attempts left.`);
     }
   }
+}
+iceQ();
+//visited
+function visitQ() {
+  let guessCount = 0;
 
-  // Check if the user has run out of attempts
-  if (attemptsRemaining === 0) {
-    // Display a message indicating that the user is out of attempts and reveal the correct answer
-    alert(`Sorry, you are out of attempts. My favorite ice cream flavor is ${myFavIceCream}.`);
-  } else {
-    // Display a message indicating that the user guessed the correct flavor and reveal their score
-    alert(`Congratulations! You guessed my favorite ice cream flavor in ${6 - attemptsRemaining} attempts. You scored ${userPoints} out of ${possiblePoints} possible point(s).`);
+  while (guessCount > 0) {
+    let guess = prompt('Have I visited more than 5 countries? Yes or No? (Answer with "Y" or "N")');
+
+    if (guess === 'y' || guess === 'Y' || guess === 'yes' || guess === 'Yes') {
+      alert('Correct! I have visited 6 countries.');
+      break;
+    } else if (guess === 'n' || guess === 'N' || guess === 'no' || guess === 'No') {
+      guessCount--;
+      alert(`Incorrect. I have visited more than 5 countries. You have ${guessCount} guesses left.`);
+    } else {
+      alert('Please answer with "Yes" or "No".');
+    }
+
+    alert('The correct answer is "Yes". Next question!.');
   }
 }
-
-// Call the iceQ function to start the game
-iceQ();
-
-
-//Food fav
+visitQ();
 function foodQ() {
   // Declare the number of attempts the user has to answer the question correctly
   let attempts = 6;
-  let favFood = ['Mexican', 'Chinese', 'Indian', 'Italian']; // answer will be lower case
+  let favFood = ['Mexican', 'Chinese', 'Indian', 'Italian', 'Japanese', 'French', 'Thai', 'Spanish', 'American', 'German', 'Middle Eastern', 'Turkish']; // answer will be lower case
   // Flag to keep track of whether the user's answer is correct
   let correct = false;
   // Loop while the user has remaining attempts and has not yet answered the question correctly
@@ -152,26 +140,11 @@ function foodQ() {
   }
 }
 foodQ();
-
-//City Born
-function birthQ() {
-  let placeOfBirth = prompt('Was I born in California?');
-  console.log(placeOfBirth);
-  if (placeOfBirth === 'y' || placeOfBirth === 'yes') {
-    alert('No, I was actually born in the Philippines but raised in America.');
-  } else if (placeOfBirth === 'n' || placeOfBirth === 'no') {
-    alert('You are correct. I was born in the Philippines and I am a first generation immigrant.');
-  } else {
-    alert('Please answer with a "yes" or "no".');
-  }
-}
-
-birthQ;
 //Sport
 // Declare the number of attempts the user has to answer the question correctly
 let attempts = 6;
 // Declare an array of favorite sports for the user to guess
-let favSports = ['basketball'];
+let favSports = ['basketball', 'football'];
 // Flag to keep track of whether the user's answer is correct
 let correct = false;
 let userPoints = 0;
@@ -182,7 +155,7 @@ while (attempts >= 1 && !correct) {
   attempts--;
 
   // Display a prompt dialog asking the user to guess the favorite sport, and store the answer in a variable
-  let sportAnswer = prompt('What is my favorite sport to watch and play? Basketball, Football, Baseball, Soccer, Tennis').toLowerCase();
+  let sportAnswer = prompt('What is my two favorite sport to watch and play? Basketball, Football, Baseball, Soccer, Tennis').toLowerCase();
   //Add the user's answer to the userAnswers array
   userAnswers.push(sportAnswer);
 
@@ -191,7 +164,7 @@ while (attempts >= 1 && !correct) {
     // Check if the user's answer matches the current element in the favSports array (ignoring case)
     if (sportAnswer === favSports[i].toLowerCase()) {
       // Display an alert message indicating that the answer is correct
-      alert('Correct! I love to watch and play basketball!');
+      alert(`Correct! I love to watch and play ${favSports[i]}.`);
       userPoints++;
       correct = true;
       break;
@@ -216,5 +189,3 @@ let answersMsg = `The correct answer is: ${favSports.join(', ')}.\n`;
 let userAnswersMsg = `Your answers were: ${userAnswers.join(', ')}.\n`;
 // Display the three message strings concatenated together in a dialog box
 alert(scoreMsg + answersMsg + userAnswersMsg);
-
-
